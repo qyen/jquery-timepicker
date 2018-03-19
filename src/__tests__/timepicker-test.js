b0d6caf9036f0ee682cf83eec0259b3a54c74bfb
Jon Thornton <thornton.jon@gmail.com>
2018-03-18 21:47:53 -0400

    Begin refactor into new Timepicker class

--- src/__tests__/timepicker-test.js
+++ src/__tests__/timepicker-test.js
@@ -1,25 +1,26 @@
 jest.dontMock("jquery").dontMock("../jquery.timepicker");
 
-const $ = require("jquery");
-require("../jquery.timepicker");
+import jQuery from 'jquery';
+require('../jquery.timepicker');
 
 const TEST_INPUT = "testInput";
 
 beforeEach(() => {
   document.body.innerHTML = `<input type="text" id="${TEST_INPUT}" />`;
+  jQuery(`#${TEST_INPUT}`).get(0).dataset = {}
 });
 
 test("timepicker initializes", () => {
-  $(`#${TEST_INPUT}`).timepicker();
+  jQuery(`#${TEST_INPUT}`).timepicker();
 });
 
 test("show single string noneOption correctly", () =>{
-  $(`#${TEST_INPUT}`).timepicker({
+  jQuery(`#${TEST_INPUT}`).timepicker({
     "noneOption": "----"
   }).timepicker('show');
 
-  $('.ui-timepicker-list li:first-child').trigger('click');
+  jQuery('.ui-timepicker-list li:first-child').trigger('click');
 
 
-  expect($(`#${TEST_INPUT}`).val()).toEqual('');
+  expect(jQuery(`#${TEST_INPUT}`).val()).toEqual('');
 });
